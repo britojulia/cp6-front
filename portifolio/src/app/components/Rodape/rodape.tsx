52c64342a55406a7768af4be6ffd775af7a6c7a3
britojulia <juliabri1234@gmail.com>
2024-10-30 12:35:07 -0300

    estruturando o proejto

---
+++ portifolio/src/app/components/Rodape/rodape.tsx
@@ -0,0 +1,12 @@
+
+export default function Rodape() {
+    return (
+    <footer>
+        <h3>FIAP</h3>
+        <p>desenvolvido por nós</p>
+        <div>
+        Gustavo / Bruno / Leonardo / Julia
+        </div>
+    </footer>
+    );
+}
---
+++ portifolio/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+    return (
+    <div>
+        <h1>404 : Not Found</h1>
+        <p>Ops! Algum erro inesperado aconteceu</p>
+    </div>
+    )
+}
