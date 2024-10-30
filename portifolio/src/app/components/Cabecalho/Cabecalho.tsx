411a6995d5c825a5dc72b2c4a412c17ec1ab8e33
brunosz2712 <brunosouza_27@yahoo.com.br>
2024-10-30 20:18:38 -0300

    Dando responsividade

--- portifolio/src/app/components/Cabecalho/Cabecalho.tsx
+++ portifolio/src/app/components/Cabecalho/Cabecalho.tsx
@@ -5,7 +5,7 @@ export default function Cabecalho() {
         <header className="bg-gray-900 text-white p-5">
             <h1 className="text-3xl text-center text-pink-400">FIAP</h1>
             <nav className="mt-4 text-center">
-                <ul className="space-x-4 inline-flex">
+                <ul className="space-y-2 sm:space-y-0 sm:space-x-4 flex flex-col sm:flex-row justify-center">
                     <li>
                         <Link href="/" className="bg-pink-500 text-white px-4 py-2 rounded-full hover:bg-pink-600 transition">
                             Home
